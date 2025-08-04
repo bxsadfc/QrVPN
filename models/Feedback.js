60d4714de05674897ec2fa537f63623e7b599f4d
Anarchy VPN <erorr55657@gmail.com>
2025-08-04 20:06:35 +0300

    Delete models directory

--- models/Feedback.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const feedbackSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  buyerIp: { type: String, required: true },
-  moderated: { type: Boolean, default: false },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Feedback', feedbackSchema);
